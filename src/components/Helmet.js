58b4fcc38354622862cef9ee01c29df799ccdf5b
Jaehwan Moon <mkch115@gmail.com>
2019-02-18 00:58:57 -0500

    fix(Helmet): Remove link to old version of fontawesome

--- src/components/Helmet.js
+++ src/components/Helmet.js
@@ -73,14 +73,7 @@ const Helmet = ({ theme = {} }) => (
             type="image/png"
             sizes="16x16"
             href={profile.favicon16.src}
-          />
-
-          <link
-            href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-            rel="stylesheet"
-            integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
-            crossOrigin="anonymous"
-          />
+          />          
         </ReactHelmet>
       );
     }}
